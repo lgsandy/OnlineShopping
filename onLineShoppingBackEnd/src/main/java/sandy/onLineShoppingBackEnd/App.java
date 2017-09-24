919bad8e355ae752efa6be43f743be032a8499a6
sandy <lgsandy543@gmail.com>
2017-09-24 13:24:39 +0530

    Project setup completed successfully

---
+++ onLineShoppingBackEnd/src/main/java/sandy/onLineShoppingBackEnd/App.java
@@ -0,0 +1,13 @@
+package sandy.onLineShoppingBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
